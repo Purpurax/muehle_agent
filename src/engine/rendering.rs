c6ae4702fad160a7fb04277a2409b1b512362c8b
Purpurax <maximer.2006@gmx.de>
2024-04-27 11:23:28 +0200

    autosave 3

--- src/engine/rendering.rs
+++ src/engine/rendering.rs
@@ -1,40 +1,39 @@
+use std::path::{self, Path};
+
 use crate::engine;
 
 use ggez::{Context, ContextBuilder, GameResult};
-use ggez::graphics::{self, Color};
+use ggez::graphics::{self, Color, Image};
 use ggez::event::{self, EventHandler};
 
-pub fn run(mut game: engine::Game) {
-    // Make a Context.
+pub fn render() {
     let (mut ctx, event_loop) = ContextBuilder::new("Mühle Agent", "Max Warkentin")
+        .add_resource_path("resources")
         .build()
         .expect("Could not create ggez context!");
 
-    // Create an instance of your event handler.
-    // Usually, you should provide it with the Context object to
-    // use when setting your game up.
-    let my_game = MyGame::new(&mut ctx, &mut game);
+    let my_game = MyGame::new(&mut ctx);
 
-    // Run!
     event::run(ctx, event_loop, my_game);
 }
 
 struct MyGame {
-    game: engine::Game
+    image: Image
 }
 
 impl MyGame {
-    pub fn new(_ctx: &mut Context, game: &mut engine::Game) -> MyGame {
+    pub fn new(_ctx: &mut Context) -> MyGame {
         // Load/create resources such as images here.
+        let image = Image::from_path(_ctx, Path::new("/assets/muehle_board.png")).unwrap();
         MyGame {
-            game: game,
+            image: image
         }
     }
 }
 
 impl EventHandler for MyGame {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
-        engine::game_loop(self.game);
+        
 
         Ok(())
     }
@@ -42,7 +41,7 @@ impl EventHandler for MyGame {
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         let mut canvas = graphics::Canvas::from_frame(ctx, Color::WHITE);
         
-
+        canvas.draw(&self.image, graphics::DrawParam::new());
 
         canvas.finish(ctx)
     }
